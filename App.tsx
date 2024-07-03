c07ca3923ef3b833d6cb2f1e94b124cf43a1adc1
yamataku0518 <truepartner312@gmail.com>
2024-07-04 00:48:46 +0900

    integrate ethers & walletConnect

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import '@walletconnect/react-native-compat'
+
+import { createWeb3Modal, defaultConfig, Web3Modal, W3mButton } from '@web3modal/ethers-react-native'
+import { ConfigOptions } from '@web3modal/ethers-react-native/lib/typescript/utils/defaultConfig'
+import { View } from 'react-native'
+
+// 1. Get projectId from https://cloud.walletconnect.com
+const projectId = 'Your Project ID'
+
+// 2. Create config
+const metadata = {
+  name: 'Web3Modal RN',
+  description: 'Web3Modal RN Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://'
+  }
+}
+
+const option : ConfigOptions = {
+  metadata,
+  extraConnectors: []
+}
+
+const config = defaultConfig(option)
+
+// 3. Define your chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+const polygon = {
+  chainId: 137,
+  name: 'Polygon',
+  currency: 'MATIC',
+  explorerUrl: 'https://polygonscan.com',
+  rpcUrl: 'https://polygon-rpc.com'
+}
+
+const chains = [mainnet, polygon]
+
+// 4. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  config,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Web3Modal />
+      <W3mButton />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
